4cc397fae644d66cd6f18755de078fe288e49400
Никита Миропольский <n.miropolskiy@corp.mail.ru>
2022-08-25 16:11:50 +0300

    it works

---
+++ pertsev.js
@@ -0,0 +1,7 @@
+var s = document.getElementsByTagName("strong");
+for(var i = 0; i < s.length; i++){
+   if (s[i].innerText.includes("Андрей Перцев")) {
+    document.getElementsByTagName("h1")[0].innerHTML += '<span style="color:red"> — Андрей Перцев</span>'
+    break;
+   }
+}
