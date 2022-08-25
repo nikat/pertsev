105f8f9d5db5072e9225fb83d4aba1322d7bc9b4
Никита Миропольский <n.miropolskiy@corp.mail.ru>
2022-08-25 18:58:13 +0300

    fix identation

--- pertsev.js
+++ pertsev.js
@@ -1,9 +1,9 @@
 window.addEventListener ("load", function() {
-var s = document.getElementsByTagName("strong");
-for(var i = 0; i < s.length; i++){
-   if (s[i].innerText.includes("Андрей Перцев")) {
-    document.getElementsByTagName("h1")[0].innerHTML += '<span style="color:red"> — Андрей Перцев</span>'
-    break;
+   var s = document.getElementsByTagName("strong");
+   for(var i = 0; i < s.length; i++){
+      if (s[i].innerText.includes("Андрей Перцев")) {
+         document.getElementsByTagName("h1")[0].innerHTML += '<span style="color:red"> — Андрей Перцев</span>'
+         break;
+      }
    }
-}
 });
