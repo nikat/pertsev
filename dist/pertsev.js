c66349f6be8451f4376749fe4501505fa85d3ed2
Никита Миропольский <n.miropolskiy@corp.mail.ru>
2022-08-28 10:56:22 +0300

    update 0.0.3 setInterval

--- dist/pertsev.js
+++ dist/pertsev.js
@@ -1,9 +1,18 @@
-window.addEventListener ("load", function() {
-   var s = document.getElementsByTagName("strong");
-   for(i in s){
-      if (s[i].innerText.includes("Андрей Перцев")) {
-         document.getElementsByTagName("h1")[0].innerHTML += '<span style="color:red"> — Андрей Перцев</span>'
-         break;
+mark = function() {
+   try {
+      if (! document.getElementsByTagName("h1")[0].textContent.includes("Андрей Перцев") ) {
+         var s = document.getElementsByTagName("strong");
+         for (i in s) {
+            if (s[i].innerText.includes("Андрей Перцев")) {
+               document.getElementsByTagName("h1")[0].innerHTML += '<span style="color:red"> — Андрей Перцев</span>'
+               break;
+            }
+         }
       }
+   } catch {
+      //
    }
-});
+}
+
+window.addEventListener("load", mark);
+var interval = setInterval(mark, 100);
